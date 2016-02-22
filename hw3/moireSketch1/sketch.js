ccfbc523f33864398c51c9e7275d9f7ff9c27e51
Jasmine <jchabra@gmail.com>
2016-02-22 08:02:56 -0500

    pushing up moire sketch 1

---
+++ hw3/moireSketch1/sketch.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  createCanvas(500,500);
+  background(255);
+  stroke(148,99,146,50);
+  noFill();
+}
+
+function draw() {
+  for (var i=0; i<500; i+=5){
+    beginShape(TRIANGLE_FAN);
+    vertex(257.5, 250);
+    vertex(457.5, i);
+    vertex(292, i);
+    vertex(57.5, i);
+    vertex(222, i);
+    vertex(57.5, i);
+    endShape();
+  }
+}
