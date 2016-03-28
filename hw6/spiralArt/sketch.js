f75a9ca9136bfc5aef114548492bed4a36b8a8c2
Jasmine <jchabra@gmail.com>
2016-03-28 12:17:58 -0400

    pushing up hw7

--- hw6/spiralArt/sketch.js
+++ hw6/spiralArt/sketch.js
@@ -5,17 +5,21 @@ function setup(){
 }
 function draw() {
   noStroke();
+  // random colors for fill
   var red=random(255);
   var blue=random(255);
   var green=random(255);
+  // the design will start in the middle of the canvas
   translate(width/2,height/2);
   angle += 0.1;
   rotate(angle);
+  // speed of the movement of the design
   frameRate(30);
   for(var i =0; i < 8; i++){
     push();
     rotate(TWO_PI * i / 8);
-    fill(red, blue, green,50);
+    fill(red, blue, green,100);
+    // using circles
     ellipse(frameCount % (width/2),0,5,5);
     pop();
   }
