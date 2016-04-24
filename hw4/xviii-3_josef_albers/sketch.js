af733e57ae662d36cab2b9ed3c15e2099160efc4
Jasmine <jchabra@gmail.com>
2016-04-24 00:15:43 -0400

    final version of part 2 of hw4

--- hw4/xviii-3_josef_albers/sketch.js
+++ hw4/xviii-3_josef_albers/sketch.js
@@ -28,11 +28,60 @@ function draw() {
   fill("#2b6f93");
   quad(215,120,320,127,310,182,215,184);
 
-// shape4 - blue box
+// shape4 - light blue box
+  noStroke();
+  fill("#8b231b");
+  rect(0,400,300,150);
+  
+  fill("#5d97c4");
+  ellipse(100,400, 300, 120);
+  
+  fill("#5d97c4");
+  ellipse(100,560, 350, 160);
+  
+// shape5 - brown box
+  noStroke();
+  fill("#592918");
+  quad(0,280,400,213,400,385,0,366);
+  stroke("#163d58");
+  strokeWeight(7);
+  fill("#592918");
+  quad(50,300,150,295,150,335,60,340);
+  
+
+// shape6 - blue box
   noStroke();
   fill("#2a709a");
-  quad(200,200,400,213,400,385,180,365);
+  quad(200,200,400,213,400,405,180,385);
+  
+  //~red leaf shape
   fill("#7e1f17");
-  // quad(215,120,320,127,310,182,215,184);
+  beginShape();
+  vertex(300, 280);
+  bezierVertex(340, 240, 380, 225, 400, 265);
+  endShape();
+  
+  stroke("#7e1f17");
+  strokeWeight(2);
+  triangle(300, 280, 400, 265, 400, 284);
+  
+  noStroke();
+  beginShape();
+  vertex(300, 280);
+  bezierVertex(338, 300, 380, 325, 400, 284);
+  endShape();
+  
+  // shape7 - coral box
+  noStroke();
+  fill("#b94c47");
+  quad(220,388,400,404,400,600,180,600);
+  stroke("#7da264");
+  strokeWeight(7);
+  fill("#b94c47");
+  quad(250,435,310,430,310,475,250,480);
+  
+  // #7da264
+
+  
   
 }
