f2bcfbdfa4e5c0b55b95d80afc7b268b73c8222e
Jasmine <jchabra@gmail.com>
2016-05-06 20:18:10 -0400

    cleaned up the code

--- finalProject/if_i_were_a_poem/sketch.js
+++ finalProject/if_i_were_a_poem/sketch.js
@@ -1,5 +1,10 @@
-// //Testing with hardcoded text
-// var lines = "Sonnet 18 \n Shall I compare thee to a summer's day? \n Thou art more lovely and more temperate: \n Rough winds do shake the darling buds of May, \n And summer's lease hath all too short a date: \n Sometime too hot the eye of heaven shines, \n And often is his gold complexion dimm'd, \n And every fair from fair sometime declines, \n By chance, or nature's changing course untrimm'd: \n But thy eternal summer shall not fade, \n Nor lose possession of that fair thou ow'st, \n Nor shall death brag thou wander'st in his shade, \n When in eternal lines to time thou grow'st, \n So long as men can breathe, or eyes can see, \n So long lives this, and this gives life to thee. \n  \n Author:William Shakespeare";
+/**
+* Project Title: IF I WERE A POEM
+* Author: Jasmine Chabra
+* Description: This poem is called "It's Dark in Here" from
+* the children's poetry book, "Where the Sidewalk Ends" where 
+* the nouns and adjectives change randomly.
+**/
 
 var lines = [];//we declare lines as an empty array 
 var words = [];
@@ -10,7 +15,6 @@ var bgImage;
 
 function preload(){
   //loadStrings returns an array of Strings from our text file
-  //you must have a local file in order for this function to work
   lines = loadStrings("assets/itsDarkInHere.txt");
   bgImage = loadImage('images/lion.png');
 }
@@ -39,7 +43,6 @@ function draw(){
   fill(129, 100, 162);
   
   push();
-  //NOTE: this text will overlap at 0,330
   fill(0);
   text("Where the Sidewalk Ends - " + '\n' + "IT'S DARK IN HERE", 0,0,width,height);
   pop();
@@ -47,8 +50,6 @@ function draw(){
   translate(0,90);
   replaceWords.length = 0;
   for(var i=0; i < words.length; i++){
-    //we check whether each parts of speech exists
-    //in our array
     if( partsOfSpeech[i] !== null){
       if(partsOfSpeech[i] === 'nn') {
         replaceWords.push(i);
